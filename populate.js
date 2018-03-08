b8b25326eeae62e1ba3e5b6c7d67b0de223cb393
Adam <as2569@njit.edu>
2018-03-07 20:36:25 -0500

    populating thumbnails with dummy data

---
+++ populate.js
@@ -0,0 +1,13 @@
+function callTwitch()
+{
+	var response = $.ajax({
+		url: 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends',
+		async: false,
+		dataType: 'JSON',
+		headers: {
+		'Client-ID': 'bz4bwfixqvekm7bq9n0ebu9rbeds6l'
+		}
+	}).responseJSON;
+	
+	return response;
+}
